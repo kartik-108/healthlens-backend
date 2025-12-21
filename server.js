d7d92d99b104dd16fd7d5c18c929df50cef7bbfa
Karthik Vijay <vijayankarthik2006@gmail.com>
2025-12-21 13:39:30 +0530

    Fixed project structure, moved server.js to root

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 👇 THIS LINE IS MUST
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("HealthLens Backend is LIVE 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
