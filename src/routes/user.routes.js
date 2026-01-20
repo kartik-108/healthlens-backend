b037f55fc5b166766e0b1d88126070396835e513
Karthik Vijay <vijayankarthik2006@gmail.com>
2026-01-20 23:20:14 +0530

    Error Fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const { protect } = require('../middleware/auth');
+const protect = require('../middleware/auth.middleware');
 
 // Update user profile
 router.put('/profile', protect, userController.updateProfile);
