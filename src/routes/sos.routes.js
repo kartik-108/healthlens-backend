2aca2786728f97d4405db14dd6fc2e46430bcf2f
Karthik Vijay <vijayankarthik2006@gmail.com>
2026-03-13 20:18:48 +0530

    fixed auth middleware path

--- src/routes/sos.routes.js
+++ src/routes/sos.routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
 
 const { triggerSOS } = require("../controllers/sos.controller");
 
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/trigger", authMiddleware, triggerSOS);
 
